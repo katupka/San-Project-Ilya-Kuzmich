f03ac146fddf15634012b10701f6c7efd87b326a
Ilya Kuzmich <ilkuz98@gmail.com>
2022-01-03 20:42:51 +0300

    weather cards 2

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {RestService} from "../../services/rest/rest.service";
 import {WeatherResponse} from "../../models/dto/weather-response";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-home-page',
@@ -10,23 +11,37 @@ import {WeatherResponse} from "../../models/dto/weather-response";
 export class HomePageComponent implements OnInit {
 
   textInput = ''
+  cols = 1;
 
   weatherList: Array<WeatherResponse> = []
 
-  constructor(private rest: RestService) {
+  constructor(private rest: RestService, private snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
-
+    this.cols = Math.floor(window.innerWidth / 300)
   }
 
   findWeather() {
     this.rest.getWeather(this.textInput).subscribe(weather => {
-      console.log(weather)
       this.weatherList.unshift(weather)
     })
 
     this.textInput = '';
+  }
+
+  deleteWeather(id: number) {
+    this.weatherList.splice(id, 1);
+    this.openSnackBar("Usunięto!")
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.cols = Math.floor(window.innerWidth / 300)
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, "Ok", {duration: 1000, horizontalPosition: 'center'});
   }
 
 }
